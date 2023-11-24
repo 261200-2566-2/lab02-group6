1cabfe502df693fa0eafae0e4f8ee84c1808ce32
640610645 <nontapan_chanadee@cmu.ac.th>
2023-11-24 12:44:30 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         AirPurifier a = new AirPurifier("Xiaomi","Pro",new double[]{300,300,689},"red");
+        System.out.println(a.brand + " " + a.model + " " + a.dimension[0] + "x" + a.dimension[1] + " x "+ a.dimension[2] + " " + a.color);
         AirPurifier b = new AirPurifier();
-        System.out.println(a.brand);
+        System.out.println(b.brand + " " + b.model + " " + b.dimension[0] + " x " + b.dimension[1] + " x " + b.dimension[2] + " " + b.color);
         a.turnOn();
         System.out.println(a.power);
         System.out.println(a.dust);
